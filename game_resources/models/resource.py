80e0d8e1d55e8c65e563f2ad91fa4d90684ad093
El dioss Abhinaya <dahalabhinaya126@gmail.com>
2025-01-13 09:09:20 +0100

    sdad

--- game_resources/models/resource.py
+++ game_resources/models/resource.py
@@ -1,8 +1,11 @@
 from odoo import models, fields, api
+from odoo.tools import float_round
+
 
 class GameResource(models.Model):
     _name = 'game.resource'
     _description = 'Game Resource'
+    _order = 'name'
 
     name = fields.Char(string='Resource Name', required=True)
     description = fields.Text(string='Description')
@@ -13,6 +16,13 @@ class GameResource(models.Model):
         ('ability', 'Ability'),
     ], string='Category', required=True)
     availability = fields.Boolean(string='Available', default=True)
+
+ currency_id = fields.Many2one('res.currency', string='Currency', 
+        default=lambda self: self.env.company.currency_id.id)
+
+    def toggle_availability(self):
+        for record in self:
+            record.availability = not record.availability
 
 class PlayerInventory(models.Model):
     _name = 'player.inventory'
